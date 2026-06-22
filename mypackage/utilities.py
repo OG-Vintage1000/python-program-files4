bd333ff1439d1794186a420da881461e9a34d01f
Brian Conley <78391712+OG-Vintage1000@users.noreply.github.com>
2026-06-21 22:41:47 -0500

    mypackage folder with modules

---
+++ mypackage/utilities.py
@@ -0,0 +1,10 @@
+import math
+
+def greet(name):
+    # name = input("What's your name? ")
+    print(f"Hello {name}!")
+print()
+
+def factorial(value):
+    # value = int(input("Choose a value: "))
+    print(f"{value}! is {math.factorial(value)}.")
