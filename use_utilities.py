18b2776f179cba31d886994594f9811e4a42444c
Brian Conley <78391712+OG-Vintage1000@users.noreply.github.com>
2026-06-21 22:40:10 -0500

    use_utilities file to use code from utilities file in mypackage folder

---
+++ use_utilities.py
@@ -0,0 +1,12 @@
+from mypackage import utilities
+
+print("Print greeting")
+gov_name = input("What's your name? ")
+utilities.greet(gov_name)
+print()
+
+print("Factorial value")
+num = int(input("Choose a value: "))
+utilities.factorial(num)
+
+# Very basic code
